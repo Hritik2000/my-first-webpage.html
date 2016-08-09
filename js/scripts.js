fb7541205ed34bd702304b006b47546beb198cf8
Ryan Vinyard <Guest@epicodus-44.local>
2016-08-09 13:53:17 -0700

    fix buttons for high and regular contrast

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $("button#contrastbutton").click(function() {
-    $("body").removeClass();
-    $("body").addClass("high-contrast")
+    $("body").addClass("high-contrast");
+    $("button#revertbutton").fadeIn("fast");
+    $("button#contrastbutton").fadeOut("fast");
 });
+});
+
+$(document).ready(function() {
+  $("button#revertbutton").click(function() {
+    $("body").removeClass("high-contrast");
+    $("button#contrastbutton").fadeIn("fast");
+    $("button#revertbutton").fadeOut("fast");
+  });
 });
